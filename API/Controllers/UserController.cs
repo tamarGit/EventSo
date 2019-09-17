6acac8c673d2844e6c05fff5472d182f8af4e2fc
tamar <tamarweitz1@gmail.com>
2019-09-17 16:13:48 +0300

    convert

---
+++ API/Controllers/UserController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using BL;
+
+
+namespace API.Controllers
+{
+    public class UserController : ApiController
+    {
+        public BL.UserBL userBL = new BL.UserBL();
+        [Route("api/User/Get")]
+        [HttpGet]
+        public Event Get(string userName, int code)
+        {
+            return userBL.
+        }
+    }
+}
---
+++ BL/UserBL.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAL;
+
+namespace BL
+{
+    class UserBL
+    {
+        public EventSoDBEntities1 DB = new EventSoDBEntities1();
+        public List<Event> GetEvents()
+        {
+            return 
+        }
+    }
+}
